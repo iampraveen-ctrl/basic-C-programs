249349fcabec589486350fd6a71d483f1e210cdb
iampraveen-ctrl <mrpraveen2330@gmail.com>
2026-07-09 16:24:29 +0530

    Delete c program directory

--- c program/exp3.c
+++
@@ -1,9 +0,0 @@
- #include<stdio.h>
- int main(){
- signed int a=2147483647;
- int b=4294967295;
- char z="adfv";
- printf("%d\n",a);
- printf("%d\n",b+1);
- printf("%d",sizeof(z));
- }
--- c program/exp4.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char a='g',b='d';
-    printf("%c",)
-}
